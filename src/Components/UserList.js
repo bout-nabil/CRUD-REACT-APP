8cd66d86827cddba39ac19bae339807b4ef836db
bout-nabil <boutachrafine.nabil@gmail.com>
2023-05-26 12:12:23 +0100

    commited data from store we use uesSelector()

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,8 +1,46 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 function UserList() {
+    //data from store we use useSelector()
+    const users = useSelector(data=>data.users);
     return(
         <div>
-            <h1>User List</h1>
-            
+            <h2>User List</h2>
+            <div>
+                <Link to="/add-user">
+                    <button>Add user</button>
+                </Link>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                        {
+                            users.map((user,index)=>{
+                                return(
+                                    <tr key={index}>
+                                        <td>{user.id}</td>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>
+                                            <button>Edit</button>
+                                            <button>Remove</button>
+                                        </td>
+                                    
+                                   </tr>
+                                )
+                            })
+                        }
+                </tbody>
+            </table>
         </div>
     )
 }
