22bf87b45f5a017d269c5ec03e45b0666e9705db
bout-nabil <boutachrafine.nabil@gmail.com>
2023-05-26 15:51:04 +0100

    commited App css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>CRUD React App</h1>
+      <h1 id='title'>CRUD React App</h1>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<UserList />} />
