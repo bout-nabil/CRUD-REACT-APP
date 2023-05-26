a504d138ca41bda6242e1927f01f8a454e2bb322
bout-nabil <boutachrafine.nabil@gmail.com>
2023-05-26 12:31:26 +0100

    commited UpdateUser

--- src/Components/UpdateUser.js
+++ src/Components/UpdateUser.js
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 
 function UpdateUser() {
-    const id = useParams();
+    const {id} = useParams()
     return(
         <div>UpdateUser {id}</div>
     )
