91124f29b0188788d2c0ca0e8a65266f218180f6
bout-nabil <boutachrafine.nabil@gmail.com>
2023-05-26 13:00:19 +0100

    commited AddUser

--- src/Components/UpdateUser.js
+++ src/Components/UpdateUser.js
@@ -1,9 +1,34 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { updateUserAction } from "../Config/actions";
 
 function UpdateUser() {
-    const {id} = useParams()
+    const {id} = useParams();
+    const dispatch = useDispatch();
+    const user = useSelector(data=>data.users.find((u)=>u.id===parseInt(id)));
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const navigate = useNavigate();
+    const handleClick = () => {
+        dispatch(updateUserAction({
+            id: id,
+            name: name,
+            email: email
+        }))
+        navigate('/');
+    }
+
     return(
-        <div>UpdateUser {id}</div>
+        <div>
+            <form>
+                <label>Name </label>
+                <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+                <label>Email </label>
+                <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+                <button onClick={handleClick}>Register</button>
+            </form>
+        </div>
     )
 }
 
